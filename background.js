7ca31045351cb7456357d7bf8c38e12f0ba10b84
Rajeev J <rajeev.jhaj@gmail.com>
2022-02-23 22:29:06 +0000

    Added a SSL checker
    Gets the website URL and sees if it starts with "HTTPS"

--- background.js
+++ background.js
@@ -4,3 +4,22 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ profileSetting });
   console.log("You are rated as a "+ profileSetting);
 });
+
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+  score=0;
+
+  url=tabs[0].url
+  urlProtocol=url.slice(0,5);
+
+  //Check if SSL certificate
+  if (urlProtocol=="https"){
+      score+=100;
+  } else{
+      score+=0;
+  }
+
+  document.getElementById("score").innerHTML =score;
+  if (score<30){
+      alert("This website looks unsafe! Be careful!");
+  }
+});
