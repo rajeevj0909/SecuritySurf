3ffe7c15c7c1f84d1abfedddd073ac4a48a2e8be
Rajeev J <rajeev.jhaj@gmail.com>
2022-04-12 19:48:53 +0100

    Created function for button to expand section

--- options.js
+++ options.js
@@ -1,7 +1,16 @@
+//Open page with About section closed
+$("#aboutSection").hide();
+
 //Begin as a beginner
 let profileSetting = 'beginner';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ profileSetting });
   console.log("You are rated as a "+ profileSetting);
+});
+
+
+//Button opens about section
+$("#aboutButton").click(function(){
+  $("#aboutSection").toggle(1000);
 });
