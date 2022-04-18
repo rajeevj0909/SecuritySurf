b6cece63b60d8695bae2df02665e42235c16fdc5
Rajeev J <rajeev.jhaj@gmail.com>
2022-04-18 19:12:39 +0100

    Created WhoIs and TrustPilot iFrames
    -Added more iFrames
    -Added protocol check (unsupported websites)
    -Added white text to options page
    -Added a report website button

--- content.js
+++ content.js
@@ -7,56 +7,57 @@ function checkScore(){
     let urlHost=window.location.hostname //URL Host
     let urlProtocol=window.location.protocol; //HTTPS or HTTP
 
-
-    //Check for SSL certificate
-    function checkSSL(score, urlProtocol){
-        let SSLused="N/A";
-        if (urlProtocol=="https:"){
-            score+=100;
-            SSLused="true";
-        }else if (urlProtocol=="http:"){ 
-            score+=5;
-            SSLused="false";
-        }
-        return [score, SSLused];
-    }
-    [score, SSLused] = checkSSL(score, urlProtocol);
-
-    //Inform user of any vulnerabilities
-    let whatToTellThem="\nSecurity Surf\n";
-    chrome.storage.sync.get("extensionOptions", function(result) {
-        let expertiseChosen= result.extensionOptions.expertiseChosen;
-        let popupOption= result.extensionOptions.popupOption;
-
-        if (popupOption=="yes"){
-            //Unsafe threshold
-            if (score<30){
-                whatToTellThem = whatToTellThem.concat("\n This website looks unsafe! Be careful!");
+    if((urlProtocol="https:")||(urlProtocol="http:")){
+        //Check for SSL certificate
+        function checkSSL(score, urlProtocol){
+            let SSLused="N/A";
+            if (urlProtocol=="https:"){
+                score+=100;
+                SSLused="true";
+            }else if (urlProtocol=="http:"){ 
+                score+=5;
+                SSLused="false";
             }
+            return [score, SSLused];
+        }
+        [score, SSLused] = checkSSL(score, urlProtocol);
 
-            //No SSL Message
-            if (SSLused=="false" && (expertiseChosen=="beginner")){
-                whatToTellThem = whatToTellThem.concat("\n     -It is very easy to see the information you send to this website!");
-            } else if (SSLused=="false" && (expertiseChosen=="expert")){
-                whatToTellThem = whatToTellThem.concat("\n     -This website uses no encryption! Data sent & recieved is in plaintext!");
+        //Inform user of any vulnerabilities
+        let whatToTellThem="\nSecurity Surf\n";
+        chrome.storage.sync.get("extensionOptions", function(result) {
+            let expertiseChosen= result.extensionOptions.expertiseChosen;
+            let popupOption= result.extensionOptions.popupOption;
+
+            if (popupOption=="yes"){
+                //Unsafe threshold
+                if (score<30){
+                    whatToTellThem = whatToTellThem.concat("\n This website looks unsafe! Be careful!");
+                }
+
+                //No SSL Message
+                if (SSLused=="false" && (expertiseChosen=="beginner")){
+                    whatToTellThem = whatToTellThem.concat("\n     -It is very easy to see the information you send to this website!");
+                } else if (SSLused=="false" && (expertiseChosen=="expert")){
+                    whatToTellThem = whatToTellThem.concat("\n     -This website uses no encryption! Data sent & recieved is in plaintext!");
+                }
             }
-        }
-        
-        //If there's something to tell them, SAY IT
-        if (whatToTellThem.length>15){
-            alert(whatToTellThem);
-        }
-    });
-
-    //Listens for popup request of data
-    function storeWebsiteInfo(websiteScore,urlHost,websiteSSL){
-        chrome.storage.sync.get("websitesVisited", function(websiteResults){ 
-            let oldResults = websiteResults.websitesVisited;
-            oldResults[urlHost]={"score":websiteScore, "TTL":Date.now() , "websiteSSL":websiteSSL};
-            chrome.storage.sync.set({"websitesVisited": oldResults});
+            
+            //If there's something to tell them, SAY IT
+            if (whatToTellThem.length>15){
+                alert(whatToTellThem);
+            }
         });
+
+        //Listens for popup request of data
+        function storeWebsiteInfo(websiteScore,urlHost,websiteSSL){
+            chrome.storage.sync.get("websitesVisited", function(websiteResults){ 
+                let oldResults = websiteResults.websitesVisited;
+                oldResults[urlHost]={"score":websiteScore, "TTL":Date.now() , "websiteSSL":websiteSSL};
+                chrome.storage.sync.set({"websitesVisited": oldResults});
+            });
+        }
+        storeWebsiteInfo(score,urlHost,SSLused);
     }
-    storeWebsiteInfo(score,urlHost,SSLused);
 }
 
 //Checks whitelist and storage before checking score
--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   let urlOfWebsite=tabs[0].url;
   let hostNameOfURL= (new URL(urlOfWebsite)).hostname;
+  let protocolOfURL= (new URL(urlOfWebsite)).protocol;
 
   chrome.storage.sync.get("websitesVisited", function(websiteResults) {
     let websiteData=websiteResults.websitesVisited[hostNameOfURL]
@@ -21,26 +22,36 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         $("#"+this.value).show();
       });
 
-      //Issues List
-      let issues=[];
-      
-      //Display score
-      $("#ratedScore").text(websiteData.score +"%");
+      //Only run if website is recognisable
+      if((protocolOfURL=="https:")||(protocolOfURL=="http:")){
+        //Issues List
+        let issues=[];
+        
+        //Display score
+        $("#ratedScore").text(websiteData.score +"%");
 
-      //Display iFrame for given URL
-      certificateURL="https://sitereport.netcraft.com/?url="+urlOfWebsite+"#ssl_table_section";
-      $("#certiciateInfoIFrame").attr("src",certificateURL);
+        //Display iFrames for given URL
+        certificateURL="https://sitereport.netcraft.com/?url="+urlOfWebsite+"#ssl_table_section";
+        $("#certiciateInfoIFrame").attr("src",certificateURL);
+        whoIsURL="https://sitereport.netcraft.com/?url="+urlOfWebsite+"#network_table";
+        $("#whoIsIFrame").attr("src",whoIsURL);
+        trustPilotURL="https://www.trustpilot.com/review/"+hostNameOfURL;
+        $("#trustPilotIFrame").attr("src",trustPilotURL);
 
-      //No SSL Message
-      if (websiteData.websiteSSL=="false" && (expertiseChosen=="beginner")){
-        issues.push("It is very easy to see the information you send to this website!");
-      } else if (websiteData.websiteSSL=="false" && (expertiseChosen=="expert")){
-        issues.push("This website uses no encryption! Data sent & recieved is in plaintext!");
+        //No SSL Message
+        if (websiteData.websiteSSL=="false" && (expertiseChosen=="beginner")){
+          issues.push("It is very easy to see the information you send to this website!");
+        } else if (websiteData.websiteSSL=="false" && (expertiseChosen=="expert")){
+          issues.push("This website uses no encryption! Data sent & recieved is in plaintext!");
+        }
+        for (item = 0; item < issues.length; item++) {
+          $("#issueList").append("<li>"+issues[item]+"</li>");
+        }
+      } else{
+        //Unsupported website
+        $("body").empty();
+        $("body").append('<h1 class="text-justify text-center">Unrecognised Website</h1>');
       }
-      for (item = 0; item < issues.length; item++) {
-        $("#issueList").append("<li>"+issues[item]+"</li>");
-      }
-
       //Save button adds site to whitelist
       $("#saveToWhitelist").click(function(){
         whiteList.push(hostNameOfURL);
@@ -56,6 +67,11 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       //Save button adds site to whitelist
       $("#openOptionsButton").click(function(){
         chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
+      });
+
+      //Save button adds site to whitelist
+      $("#reportWebsite").click(function(){
+        chrome.tabs.create({ 'url': 'https://www.ncsc.gov.uk/section/about-this-website/report-scam-website'});
       });
 
     });
